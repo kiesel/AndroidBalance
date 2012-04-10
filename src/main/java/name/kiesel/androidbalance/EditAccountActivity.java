6fdff05ec6e81745dad3534455389a0aaf4d93b9
Alex Kiesel <alex@kiesel.name>
2012-04-10 17:37:01 +0200

    Make progress for Edit form
    * also reimported hbci4java from a pre-1.7 compiler (Android only seems to support up to 1.6)
    * still experimenting

--- src/main/java/name/kiesel/androidbalance/EditAccountActivity.java
+++ src/main/java/name/kiesel/androidbalance/EditAccountActivity.java
@@ -17,6 +17,7 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import name.kiesel.androidbalance.repo.AccountRepository;
 import name.kiesel.hcbi.impl.HbciAccount;
+import name.kiesel.hcbi.impl.HbciCredentials;
 import org.kapott.hbci.manager.HBCIUtils;
 
 /**
@@ -24,57 +25,61 @@ import org.kapott.hbci.manager.HBCIUtils;
  * @author alex
  */
 public class EditAccountActivity extends Activity {
-    private Long accountId= null;
-    private AccountRepository repository= null;
-    private HbciAccount account= null;
-    
+
+    private Long accountId = null;
+    private AccountRepository repository = null;
+    private HbciAccount account = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.repository= new AccountRepository(this);
+        this.repository = new AccountRepository(this);
         this.repository.open();
-        
+
         this.setContentView(R.layout.edit_account);
-        
-        accountId= null;
+
+        accountId = null;
         if (null != savedInstanceState) {
-            accountId= (Long)savedInstanceState.getSerializable("accountId");
+            accountId = (Long) savedInstanceState.getSerializable("accountId");
         } else {
-            Bundle extras= this.getIntent().getExtras();
+            Bundle extras = this.getIntent().getExtras();
             if (null != extras) {
-                this.accountId= (Long)extras.getLong("accountId");
+                this.accountId = (Long) extras.getLong("accountId");
             }
         }
-        
+
         if (null != this.account) {
-            this.account= this.repository.byAccountId(this.accountId);
+            this.account = this.repository.byAccountId(this.accountId);
             this.fillData();
         }
-        
+
         // Fill spinner values
-        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.edit_account_hbciversions, R.id.edit_account_hbciver);
-        ((Spinner)this.findViewById(R.id.edit_account_hbciver)).setAdapter(adapter);
-        
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.edit_account_hbciversions, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ((Spinner) this.findViewById(R.id.edit_account_hbciver)).setAdapter(adapter);
+
         // Callback action when bankcode has changed
-        ((EditText)this.findViewById(R.id.edit_account_bankcode)).setOnFocusChangeListener(new OnFocusChangeListener() {
+        ((EditText) this.findViewById(R.id.edit_account_bankcode)).setOnFocusChangeListener(new OnFocusChangeListener() {
 
             @Override
             public void onFocusChange(View view, final boolean hasFocus) {
-                if (true == hasFocus) return;
-                
-                Editable bc= ((EditText)view).getText();
+                if (true == hasFocus) {
+                    return;
+                }
+
+                Editable bc = ((EditText) view).getText();
                 if (null != bc && bc.length() > 0) {
                     Log.d("Looking up host for bankcode: ", bc.toString());
-                    String host= HBCIUtils.getHBCIHostForBLZ(bc.toString());
-                    
+                    String host = HBCIUtils.getHBCIHostForBLZ(bc.toString());
+
                     Log.d("Received host: ", host);
-                    ((EditText)findViewById(R.id.edit_account_hbciurl)).setText(bc);
+                    ((EditText) findViewById(R.id.edit_account_hbciurl)).setText(bc);
                 }
             }
         });
-        
+
         // Callback for cancel-button
-        ((Button)this.findViewById(R.id.edit_account_cancel)).setOnClickListener(new OnClickListener() {
+        ((Button) this.findViewById(R.id.edit_account_cancel)).setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View view) {
@@ -82,12 +87,28 @@ public class EditAccountActivity extends Activity {
                 finish();
             }
         });
-        
+
         // Callback for save-button
-        ((Button)this.findViewById(R.id.edit_account_save)).setOnClickListener(new OnClickListener() {
+        ((Button) this.findViewById(R.id.edit_account_save)).setOnClickListener(new OnClickListener() {
+
+            private String extractString(int id) {
+                Editable e = ((EditText) findViewById(id)).getText();
+                if (null == e) {
+                    return null;
+                }
+
+                return e.toString();
+            }
 
             @Override
             public void onClick(View view) {
+                repository.createOrUpdateAccount(AccountRepository.fromValues(
+                        extractString(R.id.edit_account_aid),
+                        extractString(R.id.edit_account_bankcode),
+                        extractString(R.id.edit_account_userid),
+                        extractString(R.id.edit_account_hbciurl),
+                        "300"));// FIXME
+
                 setResult(RESULT_OK);
                 finish();
             }
@@ -95,8 +116,7 @@ public class EditAccountActivity extends Activity {
     }
 
     private void fillData() {
-        ((EditText)this.findViewById(R.id.edit_account_aid)).setText(this.account.getAccountNumber());
-        ((EditText)this.findViewById(R.id.edit_account_bankcode)).setText(this.account.getBankCode());
+        ((EditText) this.findViewById(R.id.edit_account_aid)).setText(this.account.getAccountNumber());
+        ((EditText) this.findViewById(R.id.edit_account_bankcode)).setText(this.account.getBankCode());
     }
-    
 }
--- src/main/java/name/kiesel/androidbalance/repo/AccountRepository.java
+++ src/main/java/name/kiesel/androidbalance/repo/AccountRepository.java
@@ -4,11 +4,15 @@
  */
 package name.kiesel.androidbalance.repo;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import name.kiesel.androidbalance.R;
 import name.kiesel.hcbi.impl.HbciAccount;
+import name.kiesel.hcbi.impl.HbciCredentials;
 
 /**
  *
@@ -18,13 +22,20 @@ public class AccountRepository {
     private static final String DATABASE_NAME= "accounts";
     private static final int DATABASE_VERSION= 1;
     public static final String TITLE_COLUMN = "name";
-    
+    private static final String[] ACCOUNT_COLUMNS = new String[] {
+            "_id",
+            "number",
+            "bankcode",
+            "hbciurl",
+            "hbciversion",
+            "userid",
+            "name"
+        };
+
     private final Context ctx;
     private DatabaseHelper dbh;
     private SQLiteDatabase db;
 
-
-    
     private static class DatabaseHelper extends SQLiteOpenHelper {
         
         DatabaseHelper(Context ctx) {
@@ -37,7 +48,10 @@ public class AccountRepository {
                     "_id integer primary key autoincrement, " +
                     "number text not null, " +
                     "bankcode text not null, " +
-                    "name text not null)"
+                    "hbciurl text null, " +
+                    "hbciversion text null, " +
+                    "name text not null, " +
+                    "userid text null)"
             );
             sqld.execSQL("insert into accounts (number, bankcode, name) values (\"8542130\", \"66090800\", \"Main account - BBBank\")");
             sqld.execSQL("insert into accounts (number, bankcode, name) values (\"123456\", \"66090900\", \"Second account\")");
@@ -65,15 +79,46 @@ public class AccountRepository {
     }
 
     public Cursor findAllAccounts() {
-        return this.db.query("accounts", new String[] {
-            "_id",
-            "number",
-            "bankcode",
-            "name"
-        }, null, null, null, null, null);
+        return this.db.query("accounts", ACCOUNT_COLUMNS, null, null, null, null, null);
     }
     
     public HbciAccount byAccountId(Long accountId) {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Cursor c= this.db.query("accounts", ACCOUNT_COLUMNS, "_id = ?", new String[] { String.valueOf(accountId) }, null, null, null);
+        
+        if (c.getCount() != 1) {
+            throw new IllegalArgumentException("Could not find proper account for id " + accountId);
+        }
+        
+        // FIXME
+        HbciAccount a= new HbciAccount(c.getString(1), c.getString(2));
+        a.getCredentials().setUserId(c.getString(5));
+        
+        return a;
     }
+    
+    public void updateAccount(HbciAccount a) {
+    }
+
+    public void createOrUpdateAccount(HbciAccount a) {
+        ContentValues values= new ContentValues();
+        values.put("number", a.getAccountNumber());
+        values.put("bankcode", a.getBankCode());
+        values.putNull("hbciurl");
+        values.put("hbciversion", "300");
+        values.put("name", "New account");
+        values.put("userid", a.getCredentials().getUserId());
+        
+        Long rowId= this.db.insert("accounts", null, values);
+        Log.i("Created new account w/ id ", String.valueOf(rowId));
+    }
+
+    public static HbciAccount fromValues(String accountNumber, String bankCode, String userId, String hbciUrl, String hbciVersion) {
+        HbciAccount a= new HbciAccount(accountNumber, bankCode);
+        a.setCredentials(new HbciCredentials());
+        a.getCredentials().setUserId(userId);
+
+        return a;
+    }
+    
+
 }
